2460c73611aafde8c52f5eddaf5bc3458c8b7190
Aleksander Beshkenadze <ab@caesar.team>
2019-07-01 15:04:34 +0300

    Change the release script

--- caesar/release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-helm package $DIR -d $DIR
---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PROJECT=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/${PROJECT}"
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "WORKING PROJECT is ${DIR}"
+if [ ! -d "$DIR" ]; then
+    echo ERROR: Project directory is NULL. Please recheck the variables 1>&2
+    exit 1 # terminate and indicate error
+fi
+helm package $DIR -d $DIR
+helm repo index $ROOT
