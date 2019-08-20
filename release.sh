b5e7dfba87c39a34c50d95b3d259adca35f34955
Aleksander Beshkenadze <ab@caesar.team>
2019-08-20 15:44:57 +0300

    Add the db backup script

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+SEMVER_REGEX='^([0-9]+\.){0,2}(\*|[0-9]+)$'
+
 PROJECT=$1
+VERSION=$2
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/${PROJECT}"
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "WORKING PROJECT is ${DIR}"
@@ -7,5 +11,12 @@ if [ ! -d "$DIR" ]; then
     echo ERROR: Project directory is NULL. Please recheck the variables 1>&2
     exit 1 # terminate and indicate error
 fi
-helm package $DIR -d $DIR
+
+if [[ $VERSION =~ $SEMVER_REGEX ]]; then
+ echo "INFO: Version $VERSION"
+ helm package $DIR  --version=$VERSION -d $DIR
+else
+  helm package $DIR -d $DIR
+fi
+
 helm repo index $DIR
