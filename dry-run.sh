77789c78e181dadc67c92e8b01f933f3c9e8e654
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2020-12-07 18:57:00 +0300

    Refactor the names of the files
    Signed-off-by: Aleksandr Beshkenadze <beshkenadze@gmail.com>

--- dry-run.sh
+++ dry-run.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-helm install -f values.yml --debug --dry-run --namespace=local-caesar local ./stable/caesar-server > result/result.yaml
+helm install -f values.yml --debug --dry-run --namespace=local-caesar local ./stable/caesar-web-client > result/result.yaml
