0bfc9b15586aa3e1cbd5359e836d4cece4aa7de9
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2020-12-07 17:40:41 +0300

    WIP
    Signed-off-by: Aleksandr Beshkenadze <beshkenadze@gmail.com>

--- dry-run.sh
+++ dry-run.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-helm install -f values.yml --debug --dry-run --namespace=default-caeasar-team test-caesar-team ./stable/caesar-server > result/result.yaml
+helm install -f values.yml --debug --dry-run --namespace=local-caesar local ./stable/caesar-server > result/result.yaml
